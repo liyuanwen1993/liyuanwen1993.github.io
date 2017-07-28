1a43c4dcd910486cc528a809c2cf61b4024a042a
liyuanwen <liyuanwen@youwei.me>
2017-07-28 10:06:31 +0800

    test

--- mobileTest/h5test2/node_modules/vlc-ui/src/components/button/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by admin on 2017/5/9.
- */
-import VButton from './button'
-export default VButton
--- mobileTest/h5test2/node_modules/vlc-ui/src/components/swipe/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by admin on 2017/5/11.
- */
-import Swipe from './swipe'
-export default Swipe
